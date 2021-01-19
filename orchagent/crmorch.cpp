b96b8f0e488e0e4721cf1d36c4d27868e46a9d86
Ann Pokora <apokora@juniper.net>
2021-01-19 11:09:07 -0800

    remove unneeded AVAIL attr

--- orchagent/crmorch.cpp
+++ orchagent/crmorch.cpp
@@ -56,7 +56,7 @@ const map<CrmResourceType, uint32_t> crmResSaiAvailAttrMap =
     { CrmResourceType::CRM_ACL_ENTRY, SAI_ACL_TABLE_ATTR_AVAILABLE_ACL_ENTRY },
     { CrmResourceType::CRM_ACL_COUNTER, SAI_ACL_TABLE_ATTR_AVAILABLE_ACL_COUNTER },
     { CrmResourceType::CRM_FDB_ENTRY, SAI_SWITCH_ATTR_AVAILABLE_FDB_ENTRY },
-    { CrmResourceType::CRM_MPLS_INSEG, SAI_SWITCH_ATTR_AVAILABLE_MPLS_INSEG_ENTRY },
+    { CrmResourceType::CRM_MPLS_INSEG, SAI_OBJECT_TYPE_INSEG_ENTRY },
 };
 
 const map<string, CrmResourceType> crmThreshTypeResMap =
@@ -422,22 +422,22 @@ void CrmOrch::getResAvailableCounters()
 
     for (auto &res : m_resourcesMap)
     {
-        sai_attribute_t attr;
-        attr.id = crmResSaiAvailAttrMap.at(res.first);
-
-        switch (attr.id)
+        switch (res.first)
         {
-            case SAI_SWITCH_ATTR_AVAILABLE_IPV4_ROUTE_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_IPV6_ROUTE_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_IPV4_NEXTHOP_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_IPV6_NEXTHOP_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_IPV4_NEIGHBOR_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_IPV6_NEIGHBOR_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_NEXT_HOP_GROUP_MEMBER_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_NEXT_HOP_GROUP_ENTRY:
-            case SAI_SWITCH_ATTR_AVAILABLE_FDB_ENTRY:
+            case CRM_IPV4_ROUTE:
+            case CRM_IPV6_ROUTE:
+            case CRM_IPV4_NEXTHOP:
+            case CRM_IPV6_NEXTHOP:
+            case CRM_IPV4_NEIGHBOR:
+            case CRM_IPV6_NEIGHBOR:
+            case CRM_NEXTHOP_GROUP_MEMBER:
+            case CRM_NEXTHOP_GROUP:
+            case CRM_FDB_ENTRY:
             case SAI_SWITCH_ATTR_AVAILABLE_MPLS_INSEG_ENTRY:
             {
+                sai_attribute_t attr;
+                attr.id = crmResSaiAvailAttrMap.at(res.first);
+
                 sai_status_t status = sai_switch_api->get_switch_attribute(gSwitchId, 1, &attr);
                 if (status != SAI_STATUS_SUCCESS)
                 {
@@ -450,9 +450,12 @@ void CrmOrch::getResAvailableCounters()
                 break;
             }
 
-            case SAI_SWITCH_ATTR_AVAILABLE_ACL_TABLE:
-            case SAI_SWITCH_ATTR_AVAILABLE_ACL_TABLE_GROUP:
+            case CRM_ACL_TABLE:
+            case CRM_ACL_GROUP:
             {
+                sai_attribute_t attr;
+                attr.id = crmResSaiAvailAttrMap.at(res.first);
+
                 vector<sai_acl_resource_t> resources(CRM_ACL_RESOURCE_COUNT);
 
                 attr.value.aclresource.count = CRM_ACL_RESOURCE_COUNT;
@@ -480,9 +483,12 @@ void CrmOrch::getResAvailableCounters()
                 break;
             }
 
-            case SAI_ACL_TABLE_ATTR_AVAILABLE_ACL_ENTRY:
-            case SAI_ACL_TABLE_ATTR_AVAILABLE_ACL_COUNTER:
+            case CRM_ACL_ENTRY:
+            case CRM_ACL_COUNTER:
             {
+                sai_attribute_t attr;
+                attr.id = crmResSaiAvailAttrMap.at(res.first);
+
                 for (auto &cnt : res.second.countersMap)
                 {
                     sai_status_t status = sai_acl_api->get_acl_table_attribute(cnt.second.id, 1, &attr);
@@ -494,6 +500,22 @@ void CrmOrch::getResAvailableCounters()
 
                     cnt.second.availableCounter = attr.value.u32;
                 }
+
+                break;
+            }
+
+            case CRM_MPLS_INSEG:
+            {
+                sai_object_type_t objType = crmResSaiAvailAttrMap.at(res.first);
+                uint64_t availCount = 0;
+                sai_status_t status = sai_object_type_get_availability(gSwitchId, objType, 0, nullptr, &availCount);
+                if (status != SAI_STATUS_SUCCESS)
+                {
+                    SWSS_LOG_ERROR("Failed to get switch attribute %u , rv:%d", attr.id, status);
+                    break;
+                }
+
+                res.second.countersMap[CRM_COUNTERS_TABLE_KEY].availableCounter = (uint32_t)availCount;
 
                 break;
             }
