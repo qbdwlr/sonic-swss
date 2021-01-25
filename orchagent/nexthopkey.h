7b138a8baa3116ca12eba00077523fe8223ac54a
Ann Pokora <apokora@juniper.net>
2021-01-25 11:22:46 -0800

    fixes from Metaswitch

--- orchagent/nexthopkey.h
+++ orchagent/nexthopkey.h
@@ -9,6 +9,7 @@
 #define NH_DELIMITER '@'
 #define NHG_DELIMITER ','
 #define VRF_PREFIX "Vrf"
+#define DEFAULT_ECMP_NH_WEIGHT 1
 extern IntfsOrch *gIntfsOrch;
 
 struct NextHopKey
@@ -18,9 +19,9 @@ struct NextHopKey
     string              alias;          // incoming interface alias
     uint8_t             weight;         // NH weight for NHGs
 
-    NextHopKey() = default;
-    NextHopKey(const std::string &ipstr, const std::string &alias) : ip_address(ipstr), alias(alias) {}
-    NextHopKey(const IpAddress &ip, const std::string &alias) : ip_address(ip), alias(alias) {}
+    NextHopKey() : weight(DEFAULT_ECMP_NH_WEIGHT) {}
+    NextHopKey(const std::string &ipstr, const std::string &alias) : ip_address(ipstr), alias(alias), weight(DEFAULT_ECMP_NH_WEIGHT) {}
+    NextHopKey(const IpAddress &ip, const std::string &alias) : ip_address(ip), alias(alias), weight(DEFAULT_ECMP_NH_WEIGHT) {}
     NextHopKey(const std::string &str)
     {
         if (str.find(NHG_DELIMITER) != string::npos)
@@ -39,7 +40,7 @@ struct NextHopKey
         {
             ip_str = str;
         }
-        auto keys = tokenize(str, NH_DELIMITER);
+        auto keys = tokenize(ip_str, NH_DELIMITER);
         if (keys.size() == 1)
         {
             ip_address = keys[0];
@@ -62,7 +63,7 @@ struct NextHopKey
     }
     const std::string to_string() const
     {
-        string str;
+        std::string str;
         if (!label_stack.empty())
         {
             str += label_stack.to_string();
